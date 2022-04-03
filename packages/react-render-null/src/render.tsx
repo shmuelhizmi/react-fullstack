98f463c1f4d157f88b03e0d582e29431cc5bd217
Shmuel Hizmi <48323313+shmuelhizmi@users.noreply.github.com>
2022-04-03 20:24:19 +0000

    update react renderer to use react tesrt renderer

--- packages/react-render-null/src/render.tsx
+++ packages/react-render-null/src/render.tsx
@@ -1,33 +1,12 @@
-import React from "react";
-import { TinyEmitter } from "tiny-emitter";
-import rendererLegacy from "./rendererBackend";
+import { create } from "react-test-renderer";
 
-export const RenderBase = (element: JSX.Element) => {
-  return rendererLegacy.updateContainer(
-    element,
-    rendererLegacy.createContainer({}, 0, false, null),
-    null,
-    () => {}
-  );
-};
 
-const RenderApp = (props: {
-  children: JSX.Element;
-  emitter: TinyEmitter;
-}): JSX.Element => {
-  const [isStopped, setIsStopped] = React.useState(false);
-  React.useEffect(() => {
-    props.emitter.on("stop", () => setIsStopped(true));
-    props.emitter.on("continue", () => setIsStopped(false));
-  }, []);
-  return isStopped ? <></> : props.children;
-};
+
 
 export const Render = (element: JSX.Element) => {
-  const emitter = new TinyEmitter();
-  RenderBase(<RenderApp emitter={emitter}>{element}</RenderApp>);
+  let tree = create(element);
   return {
-    stop: () => emitter.emit("stop"),
-    continue: () => emitter.emit("continue"),
+    stop: () => tree.unmount(),
+    continue: () => tree = create(element)
   };
 };
